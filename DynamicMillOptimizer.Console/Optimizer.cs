f9d2ddd5772dc4b871d12c036791a54d66d4409c
Adam Swanson <adam.swanson.98@gmail.com>
2025-02-12 21:29:31 -0500

    Get optimizer working

---
+++ DynamicMillOptimizer.Console/Optimizer.cs
@@ -0,0 +1,80 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DynamicMillOptimizer.Console;
+
+public class Optimizer
+{
+    /// <summary>
+    /// Matches lines in the format X123.456 or Y123.456
+    /// </summary>
+    private readonly Regex _linePattern = new(@"^([XY])(\d*\.\d*)$");
+        
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="lines"></param>
+    /// <returns></returns>
+    public string[] Optimize(string[] lines)
+    {
+        List<string> output = [];
+        List<string> optimizedLinesBuffer = [];
+        string? optimizedAxis = null;
+        
+        foreach (var currentLine in lines)
+        {
+            // Check if the current line can be optimized
+            var match = _linePattern.Match(currentLine);
+
+            if (!match.Success)
+            {
+                // Current line cannot be optimized.
+                
+                WriteAnyOptimizedLinesToBuffer(optimizedLinesBuffer, output);
+                
+                output.Add(currentLine);
+                continue;
+            }
+            
+            // Current line can be optimized.
+
+            // Determine if the current line is for X or Y
+            var axis = match.Groups[1].Value;
+
+            if (axis == optimizedAxis)
+            {
+                // The line matches the existing group of optimized lines. Add it to the list.
+                optimizedLinesBuffer.Add(currentLine);
+            }
+            else
+            {
+                // The line matches the existing group of optimized lines. Write the current buffer to the output, clear it, and add the current line to the new group of optimized lines.
+                WriteAnyOptimizedLinesToBuffer(optimizedLinesBuffer, output);
+                optimizedAxis = axis;
+                optimizedLinesBuffer.Add(currentLine);
+            }
+        }
+
+        return output.ToArray();
+    }
+
+    private static void WriteAnyOptimizedLinesToBuffer(List<string> optimizedLinesBuffer, List<string> output)
+    {
+        if (optimizedLinesBuffer.Any())
+        {
+            // There is a group of lines that needs to be optimized and copied to the output list.
+            if (optimizedLinesBuffer.Count == 1)
+            {
+                output.Add(optimizedLinesBuffer.Single());
+            }
+            else
+            {
+                // We'll assume that the tool is moving in a straight direction.
+                // In this case, we only want the first and last coordinates in the group.
+                output.AddRange([optimizedLinesBuffer.First(), optimizedLinesBuffer.Last()]);
+            }
+                    
+            // Clear the buffer in preparation for the next group of lines we encounter.
+            optimizedLinesBuffer.Clear();
+        }
+    }
+}
--- DynamicMillOptimizer.Console/Program.cs
+++ DynamicMillOptimizer.Console/Program.cs
@@ -1,3 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using DynamicMillOptimizer.Console;
+
+// Get input file
+Console.Write("Path to file: ");
+var input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("Invalid file path. Exiting...");
+    return 1;
+}
+
+var filepath = input.Replace("\"", "");
+
+var file = new FileInfo(filepath);
+
+if (!file.Exists)
+{
+    Console.WriteLine("File not found. Exiting...");
+    return 1;
+}
+
+// Read the file
+Console.WriteLine("Reading...");
+
+var lines = await File.ReadAllLinesAsync(file.FullName);
+
+// Optimize it
+var optimizedLines = new Optimizer().Optimize(lines);
+
+if (optimizedLines.Length == lines.Length)
+{
+    Console.WriteLine("File is already optimized. Exiting...");
+    return 0;
+}
+
+Console.WriteLine("Optimized from {0} lines to {1}.", lines.Length, optimizedLines.Length);
+
+var optimizedFileName = $"{Path.GetFileNameWithoutExtension(file.Name)}-optimized.txt";
+var optimizedFilePath = Path.Combine(file.DirectoryName, optimizedFileName);
+
+// Save to new file
+await File.WriteAllLinesAsync(optimizedFilePath, optimizedLines);
+
+Console.WriteLine("Saved to file: {0}", optimizedFilePath);
+
+return 0;
