a7a890e834619024680c66e98d7fd5ee92185ae5
Adam Swanson <adam.swanson.98@gmail.com>
2025-02-25 19:10:15 -0500

    Use explicit class in Program.cs

--- DynamicMillOptimizer.Console/Program.cs
+++ DynamicMillOptimizer.Console/Program.cs
@@ -1,50 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-using DynamicMillOptimizer.Core;
+﻿using DynamicMillOptimizer.Core;
 using DynamicMillOptimizer.Core.Commands;
 using DynamicMillOptimizer.Core.Commands.Optimizers;
 
-// Get input file
-Console.Write("Path to file: ");
-var input = Console.ReadLine();
+namespace DynamicMillOptimizer.Console;
 
-if (string.IsNullOrWhiteSpace(input))
+using Console = System.Console;
+
+internal static class Program
 {
-    Console.WriteLine("Invalid file path. Exiting...");
-    return 1;
+    public static async Task<int> Main(string[] args)
+    {
+        // Get input file
+        Console.Write("Path to file: ");
+        var input = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Invalid file path. Exiting...");
+            return 1;
+        }
+
+        var filepath = input.Replace("\"", "");
+
+        var file = new FileInfo(filepath);
+
+        if (!file.Exists)
+        {
+            Console.WriteLine("File not found. Exiting...");
+            return 1;
+        }
+
+        // Read the file
+        Console.WriteLine("Reading...");
+
+        var lines = await File.ReadAllLinesAsync(file.FullName);
+
+        // Optimize it
+        var optimizedLines = new FileOptimizer(new CommandParser(), new SingleAxisOptimizer()).Optimize(lines);
+
+        if (optimizedLines.Length == lines.Length)
+        {
+            Console.WriteLine("File is already optimized. Exiting...");
+            return 0;
+        }
+
+        Console.WriteLine("Optimized from {0} lines to {1}.", lines.Length, optimizedLines.Length);
+
+        var optimizedFileName = $"{Path.GetFileNameWithoutExtension(file.Name)}-optimized.txt";
+        var optimizedFilePath = Path.Combine(file.DirectoryName, optimizedFileName);
+
+        // Save to new file
+        await File.WriteAllLinesAsync(optimizedFilePath, optimizedLines);
+
+        Console.WriteLine("Saved to file: {0}", optimizedFilePath);
+
+        return 0;
+    }
 }
-
-var filepath = input.Replace("\"", "");
-
-var file = new FileInfo(filepath);
-
-if (!file.Exists)
-{
-    Console.WriteLine("File not found. Exiting...");
-    return 1;
-}
-
-// Read the file
-Console.WriteLine("Reading...");
-
-var lines = await File.ReadAllLinesAsync(file.FullName);
-
-// Optimize it
-var optimizedLines = new FileOptimizer(new CommandParser(), new SingleAxisOptimizer()).Optimize(lines);
-
-if (optimizedLines.Length == lines.Length)
-{
-    Console.WriteLine("File is already optimized. Exiting...");
-    return 0;
-}
-
-Console.WriteLine("Optimized from {0} lines to {1}.", lines.Length, optimizedLines.Length);
-
-var optimizedFileName = $"{Path.GetFileNameWithoutExtension(file.Name)}-optimized.txt";
-var optimizedFilePath = Path.Combine(file.DirectoryName, optimizedFileName);
-
-// Save to new file
-await File.WriteAllLinesAsync(optimizedFilePath, optimizedLines);
-
-Console.WriteLine("Saved to file: {0}", optimizedFilePath);
-
-return 0;
